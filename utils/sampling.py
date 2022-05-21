e8faf46123a772f4fbfbfcec7e4aee126f1aaec2
FedBKD <97731879+FedBKD@users.noreply.github.com>
2022-05-21 16:58:42 +0800

    Update sampling.py

--- utils/sampling.py
+++ utils/sampling.py
@@ -2,102 +2,11 @@
 # -*- coding: utf-8 -*-
 # Python version: 3.6
 
-import math
 import random
 import numpy as np
 import torch
-import copy
-
-def noniid_global(dataset, num_users, shard_per_user, num_classes, rand_set_all=[], testb=False):
-    """
-    Sample non-I.I.D client data from MNIST dataset
-    :param dataset:
-    :param num_users:
-    :return:
-    """
-    np.random.seed(10086)
-    # np.random.seed(87684)
-    dict_users = {i: np.array([], dtype='int64') for i in range(num_users)}
-
-    idxs_dict = {}
-    idxs_global = []
-    count = 0
-    for i in range(len(dataset)):
-        label = torch.tensor(dataset.targets[i]).item()
-        if label < num_classes and label not in idxs_dict.keys():
-            idxs_dict[label] = []
-        if label < num_classes:
-            idxs_dict[label].append(i)
-            count += 1
-    for k, v in idxs_dict.items():
-        # global_sample_id = np.random.choice(range(len(v)), len(v)//50, replace=False)
-        global_sample_id = np.random.choice(range(len(v)), math.ceil(len(v) * 0.02), replace=False)
-        global_sample_idx = [v[i] for i in global_sample_id]
-        idxs_dict[k] = list(set(v)-set(global_sample_idx))
-        idxs_global.extend(global_sample_idx)
-        count -= len(global_sample_idx)
-    np.random.shuffle(idxs_global)
-
-    shard_per_class = int(shard_per_user * num_users / num_classes)
-    samples_per_user = int( count/num_users )
-    # whether to sample more test samples per user
-    if (samples_per_user < 100):
-        double = True
-    else:
-        double = False
-
-    for label in idxs_dict.keys():
-        x = idxs_dict[label]
-        num_leftover = len(x) % shard_per_class
-        leftover = x[-num_leftover:] if num_leftover > 0 else []
-        x = np.array(x[:-num_leftover]) if num_leftover > 0 else np.array(x)
-        x = x.reshape((shard_per_class, -1))
-        x = list(x)
-
-        for i, idx in enumerate(leftover):
-            x[i] = np.concatenate([x[i], [idx]])
-        idxs_dict[label] = x
-
-    if len(rand_set_all) == 0:
-        rand_set_all = list(range(num_classes)) * shard_per_class
-        np.random.shuffle(rand_set_all)
-        rand_set_all = np.array(rand_set_all).reshape((num_users, -1))
-
-    # divide and assign
-    for i in range(num_users):
-        if double:
-            rand_set_label = list(rand_set_all[i]) * 50
-        else:
-            rand_set_label = rand_set_all[i]
-        rand_set = []
-        for label in rand_set_label:
-            try:
-                idx = np.random.choice(len(idxs_dict[label]), replace=False)
-            except:
-                print('zzzz')
-            if (samples_per_user < 100 or testb):
-                rand_set.append(idxs_dict[label][idx])
-            else:
-                rand_set.append(idxs_dict[label].pop(idx))
-        dict_users[i] = np.concatenate(rand_set)
-
-    test = []
-    for key, value in dict_users.items():
-        x = np.unique(torch.tensor(dataset.targets)[value])
-        test.append(value)
-    test = np.concatenate(test)
-
-    return dict_users, rand_set_all, idxs_global
-
 
 def noniid(dataset, num_users, shard_per_user, num_classes, rand_set_all=[], testb=False):
-    """
-    Sample non-I.I.D client data from MNIST dataset
-    :param dataset:
-    :param num_users:
-    :return:
-    """
-    np.random.seed(10086)
     dict_users = {i: np.array([], dtype='int64') for i in range(num_users)}
 
     idxs_dict = {}
@@ -132,7 +41,7 @@ def noniid(dataset, num_users, shard_per_user, num_classes, rand_set_all=[], tes
 
     if len(rand_set_all) == 0:
         rand_set_all = list(range(num_classes)) * shard_per_class
-        np.random.shuffle(rand_set_all)
+        random.shuffle(rand_set_all)
         rand_set_all = np.array(rand_set_all).reshape((num_users, -1))
 
     # divide and assign
@@ -143,11 +52,8 @@ def noniid(dataset, num_users, shard_per_user, num_classes, rand_set_all=[], tes
             rand_set_label = rand_set_all[i]
         rand_set = []
         for label in rand_set_label:
-            try:
-                idx = np.random.choice(len(idxs_dict[label]), replace=False)
-            except:
-                print('zzzz')
-            if (samples_per_user < 100 or testb):
+            idx = np.random.choice(len(idxs_dict[label]), replace=False)
+            if (samples_per_user < 100 and testb):
                 rand_set.append(idxs_dict[label][idx])
             else:
                 rand_set.append(idxs_dict[label].pop(idx))
