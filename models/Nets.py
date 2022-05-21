87f9927e705d65729abb7849fd87b8a98e62be4f
FedBKD <97731879+FedBKD@users.noreply.github.com>
2022-05-21 16:54:32 +0800

    Update Nets.py

--- models/Nets.py
+++ models/Nets.py
@@ -1,4 +1,4 @@
-# Modified from: https://github.com/pliang279/LG-FedAvg/blob/master/models/Nets.py
+# Modified from: https://github.com/lgcollins/FedRep.git
 
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
@@ -7,15 +7,8 @@
 import torch
 from torch import nn
 import torch.nn.functional as F
-from torchvision import models
-import json
-import numpy as np
 from models.language_utils import get_word_emb_arr
 
-if torch.cuda.is_available():
-    torch.cuda.manual_seed(20)
-torch.manual_seed(20)
-# np.random.seed(seed)
 torch.backends.cudnn.benchmark = False
 torch.backends.cudnn.deterministic = True
 
@@ -75,7 +68,7 @@ class CNNCifar(nn.Module):
         self.fc2 = nn.Linear(120, 64)
         self.fc3 = nn.Linear(64, args.num_classes)
         self.cls = args.num_classes
-        # add
+        # # add
         self.drop = nn.Dropout(0.6)
 
         self.weight_keys = [['fc1.weight', 'fc1.bias'],
@@ -92,6 +85,7 @@ class CNNCifar(nn.Module):
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         x = self.drop(self.fc3(x))
+        # x = self.fc3(self.drop(x))
         return F.log_softmax(x, dim=1)
 
 class CNNCifarGlobal(nn.Module):
@@ -131,8 +125,8 @@ class CNNCifarMulti(nn.Module):
         self.fc2 = nn.Linear(120, 64)
         self.fc3 = nn.Linear(64, args.num_classes)
         self.cls = args.num_classes
-        # add
-        self.drop = nn.Dropout(0.6)
+        # # add
+        # self.drop = nn.Dropout(0.6)
 
         self.weight_keys = [['fc1.weight', 'fc1.bias'],
                             ['fc2.weight', 'fc2.bias'],
@@ -141,13 +135,14 @@ class CNNCifarMulti(nn.Module):
                             ['conv1.weight', 'conv1.bias'],
                             ]
 
-    def forward(self, x):
+    def forward(self, x, tem=1):
         x = self.pool(F.relu(self.conv1(x)))
         x = self.pool(F.relu(self.conv2(x)))
         x = x.view(-1, 64 * 5 * 5)
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
-        output = self.drop(self.fc3(x))
+        # output = self.drop(self.fc3(x))/tem
+        output = self.fc3(x)/tem
         return x, F.log_softmax(output, dim=1)
 
 class CNNCifar100Multi(nn.Module):
@@ -169,13 +164,13 @@ class CNNCifar100Multi(nn.Module):
                             ['conv1.weight', 'conv1.bias'],
                             ]
 
-    def forward(self, x):
+    def forward(self, x, tem=1):
         x = self.pool(F.relu(self.conv1(x)))
         x = self.pool(F.relu(self.conv2(x)))
         x = x.view(-1, 128 * 5 * 5)
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
-        output = self.fc3(self.drop(x))
+        output = self.fc3(self.drop(x))/tem
         return x, F.log_softmax(output, dim=1)
 
 class MLPMulti(nn.Module):
@@ -194,7 +189,7 @@ class MLPMulti(nn.Module):
                             ['layer_out.weight', 'layer_out.bias']
                             ]
 
-    def forward(self, x):
+    def forward(self, x, tem=1):
         x = x.view(-1, x.shape[1]*x.shape[-2]*x.shape[-1])
         x = self.layer_input(x)
         x = self.relu(x)
@@ -202,67 +197,24 @@ class MLPMulti(nn.Module):
         x = self.relu(x)
         x = self.layer_hidden2(x)
         x = self.relu(x)
-        output = self.layer_out(x)
+        output = self.layer_out(x)/tem
         return x, self.softmax(output)
 
-
 class RNNSentMulti(nn.Module):
-    """
-    Container module with an encoder, a recurrent module, and a decoder.
-    Modified by: Hongyi Wang from https://github.com/pytorch/examples/blob/master/word_language_model/model.py
-    """
-
-    def __init__(self, args, rnn_type, ntoken, ninp, nhid, nlayers, dropout=0.5, tie_weights=False, emb_arr=None):
+    def __init__(self, args, dropout=0.5):
         super(RNNSentMulti, self).__init__()
-        # VOCAB_DIR = 'models/embs.json'
-        VOCAB_DIR = '/home/FedRep/models/embs.json'
-        emb, self.indd, vocab = get_word_emb_arr(VOCAB_DIR)
-        self.encoder = torch.tensor(emb).to(args.device)
-
-        if rnn_type in ['LSTM', 'GRU']:
-            self.rnn = getattr(nn, rnn_type)(ninp, nhid, nlayers)
-        else:
-            try:
-                nonlinearity = {'RNN_TANH': 'tanh', 'RNN_RELU': 'relu'}[rnn_type]
-            except KeyError:
-                raise ValueError("""An invalid option for `--model` was supplied,
-                                 options are ['LSTM', 'GRU', 'RNN_TANH' or 'RNN_RELU']""")
-            self.rnn = nn.RNN(ninp, nhid, nlayers, nonlinearity=nonlinearity, dropout=dropout)
-        self.fc = nn.Linear(nhid, 10)
-        self.decoder = nn.Linear(10, ntoken)
-
-        # "Using the Output Embedding to Improve Language Models" (Press & Wolf 2016)
-        # https://arxiv.org/abs/1608.05859
-        # and
-        # "Tying Word Vectors and Word Classifiers: A Loss Framework for Language Modeling" (Inan et al. 2016)
-        # https://arxiv.org/abs/1611.01462
-        if tie_weights:
-            if nhid != ninp:
-                raise ValueError('When using the tied flag, nhid must be equal to emsize')
-            self.decoder.weight = self.encoder.weight
-
+        self.args = args
+        self.rnn = getattr(nn, 'LSTM')(25, 128, 1)
+        self.fc = nn.Linear(128, 10)
         self.drop = nn.Dropout(dropout)
-        self.init_weights()
-        self.rnn_type = rnn_type
-        self.nhid = nhid
-        self.nlayers = nlayers
-        self.device = args.device
+        self.decoder = nn.Linear(10, 2)
+        self.nlayers = 1
+        self.nhid = 128
 
-    def init_weights(self):
-        initrange = 0.1
-        self.fc.bias.data.zero_()
-        self.fc.weight.data.uniform_(-initrange, initrange)
-        self.decoder.bias.data.zero_()
-        self.decoder.weight.data.uniform_(-initrange, initrange)
-
-    def forward(self, input, hidden):
-        input = torch.transpose(input, 0, 1)
-        emb = torch.zeros((25, 4, 300))
-        for i in range(25):
-            for j in range(4):
-                emb[i, j, :] = self.encoder[input[i, j], :]
-        emb = emb.to(self.device)
-        emb = emb.view(300, 4, 25)
+    def forward(self, emb, hidden=None):
+        if hidden is None:
+            hidden = self.init_hidden(1)
+        emb = emb.view(300, 1, 25)
         self.rnn.flatten_parameters()
         output, hidden = self.rnn(emb, hidden)
         output = F.relu(self.fc(output))
@@ -272,37 +224,8 @@ class RNNSentMulti(nn.Module):
 
     def init_hidden(self, bsz):
         weight = next(self.parameters())
-        if self.rnn_type == 'LSTM':
-            return (weight.new_zeros(self.nlayers, bsz, self.nhid),
-                    weight.new_zeros(self.nlayers, bsz, self.nhid))
-        else:
-            return weight.new_zeros(self.nlayers, bsz, self.nhid)
-
-class CNNCifar100Global(nn.Module):
-    def __init__(self, args):
-        super(CNNCifar100Global, self).__init__()
-        self.conv1 = nn.Conv2d(3, 64, 5)
-        self.pool = nn.MaxPool2d(2, 2)
-        self.drop = nn.Dropout(0.6)
-        self.conv2 = nn.Conv2d(64, 128, 5)
-        self.fc1 = nn.Linear(128 * 5 * 5, 256)
-        self.fc2 = nn.Linear(256, 128)
-        self.cls = args.num_classes
-
-        self.weight_keys = [['fc1.weight', 'fc1.bias'],
-                            ['fc2.weight', 'fc2.bias'],
-                            ['fc3.weight', 'fc3.bias'],
-                            ['conv2.weight', 'conv2.bias'],
-                            ['conv1.weight', 'conv1.bias'],
-                            ]
-
-    def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = x.view(-1, 128 * 5 * 5)
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        return x
+        return (weight.new_zeros(self.nlayers, bsz, self.nhid),
+                weight.new_zeros(self.nlayers, bsz, self.nhid))
 
 class CNNCifar100(nn.Module):
     def __init__(self, args):
